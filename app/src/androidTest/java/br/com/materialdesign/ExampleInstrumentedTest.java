7c8ef2eec280196a0f6d4bbe34a91f1412176a6b
Gustavo <gustavoterras@gmail.com>
2016-09-16 16:08:28 -0300

    screenshot

--- app/src/androidTest/java/br/com/materialdesign/ExampleInstrumentedTest.java
+++ app/src/androidTest/java/br/com/materialdesign/ExampleInstrumentedTest.java
@@ -1,13 +1,17 @@
 package br.com.materialdesign;
 
-import android.content.Context;
-import android.support.test.InstrumentationRegistry;
+import android.support.test.filters.LargeTest;
+import android.support.test.rule.ActivityTestRule;
 import android.support.test.runner.AndroidJUnit4;
 
+import org.junit.Before;
+import org.junit.Rule;
 import org.junit.Test;
 import org.junit.runner.RunWith;
 
-import static org.junit.Assert.*;
+import static android.support.test.espresso.Espresso.onView;
+import static android.support.test.espresso.action.ViewActions.click;
+import static android.support.test.espresso.matcher.ViewMatchers.withId;
 
 /**
  * Instrumentation test, which will execute on an Android device.
@@ -15,12 +19,18 @@ import static org.junit.Assert.*;
  * @see <a href="http://d.android.com/tools/testing">Testing documentation</a>
  */
 @RunWith(AndroidJUnit4.class)
+@LargeTest
 public class ExampleInstrumentedTest {
-    @Test
-    public void useAppContext() throws Exception {
-        // Context of the app under test.
-        Context appContext = InstrumentationRegistry.getTargetContext();
 
-        assertEquals("br.com.materialdesign", appContext.getPackageName());
+    @Rule
+    public ActivityTestRule<MainActivity> mainActivityTestRule = new ActivityTestRule<>(MainActivity.class);
+
+    @Before
+    public void initVariables() {
+    }
+
+    @Test
+    public void testCaseClickFloatActionButton() {
+        onView(withId(R.id.fab)).perform(click());
     }
 }
--- app/src/main/java/br/com/materialdesign/ScrollActivity.java
+++ app/src/main/java/br/com/materialdesign/ScrollActivity.java
@@ -1,5 +1,8 @@
 package br.com.materialdesign;
 
+import android.animation.Animator;
+import android.graphics.Color;
+import android.graphics.drawable.ColorDrawable;
 import android.support.design.widget.AppBarLayout;
 import android.support.design.widget.CollapsingToolbarLayout;
 import android.support.v7.app.AppCompatActivity;
@@ -8,10 +11,14 @@ import android.support.v7.widget.LinearLayoutManager;
 import android.support.v7.widget.RecyclerView;
 import android.support.v7.widget.Toolbar;
 import android.view.View;
+import android.view.ViewAnimationUtils;
 import android.view.ViewGroup;
 import android.widget.TextView;
 
 public class ScrollActivity extends AppCompatActivity {
+
+    static String baconTitle = "Bacon";
+    static String baconText = "Bacon ipsum dolor amet pork belly meatball kevin spare ribs. Frankfurter swine corned beef meatloaf, strip steak.";
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
@@ -33,13 +40,23 @@ public class ScrollActivity extends AppCompatActivity {
 
             @Override
             public void onBindViewHolder(ViewHolder viewHolder, int position) {
-                viewHolder.text1.setText("Bacon");
-                viewHolder.text2.setText("Bacon ipsum dolor amet pork belly meatball kevin spare ribs. Frankfurter swine corned beef meatloaf, strip steak.");
+                viewHolder.text1.setText(baconTitle);
+                viewHolder.text2.setText(baconText);
             }
 
             @Override
             public int getItemCount() {
                 return 30;
+            }
+
+            @Override
+            public long getItemId(int position) {
+                return position;
+            }
+
+            @Override
+            public void onAttachedToRecyclerView(RecyclerView recyclerView) {
+                super.onAttachedToRecyclerView(recyclerView);
             }
         });
     }
@@ -71,14 +88,36 @@ public class ScrollActivity extends AppCompatActivity {
         });
     }
 
-    private static class ViewHolder extends RecyclerView.ViewHolder {
+    private static class ViewHolder extends RecyclerView.ViewHolder implements View.OnClickListener {
         TextView text1;
         TextView text2;
+        int pink;
+        int white;
 
         ViewHolder(View itemView) {
             super(itemView);
             text1 = (TextView) itemView.findViewById(android.R.id.text1);
             text2 = (TextView) itemView.findViewById(android.R.id.text2);
+
+            if (pink == 0)
+                pink = itemView.getContext().getResources().getColor(R.color.colorAccent);
+            if (white == 0)
+                white = itemView.getContext().getResources().getColor(R.color.background_material_light);
+
+            itemView.setOnClickListener(this);
+        }
+
+        @Override
+        public void onClick(View view) {
+            boolean isVeggie = view.getBackground() != null && ((ColorDrawable) view.getBackground()).getColor() == pink;
+
+            int finalRadius = (int) Math.hypot(view.getWidth() / 2, view.getHeight() / 2);
+            Animator anim = ViewAnimationUtils.createCircularReveal(view, view.getWidth() / 2, view.getHeight() / 2, 0, finalRadius);
+
+            text1.setTextColor(isVeggie ? Color.BLACK : white);
+            text2.setTextColor(isVeggie ? Color.BLACK : white);
+            view.setBackgroundColor(isVeggie ? white : pink);
+            anim.start();
         }
     }
 }
